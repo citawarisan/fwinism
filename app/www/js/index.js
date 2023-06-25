037bffb72e9c735e604758265661eed4cefb4943
Gary <37288203+garylkz@users.noreply.github.com>
2023-06-26 02:47:38 +0800

    Let me dump the whole project here

---
+++ app/www/js/index.js
@@ -0,0 +1,10 @@
+const s = document.querySelector("#status");
+
+
+document.addEventListener('deviceready', onDeviceReady, false);
+
+function onDeviceReady() {
+    // Cordova is now initialized. Have fun!
+
+    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+}
